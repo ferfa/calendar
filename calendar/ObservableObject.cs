501777e3d71580653fa26cb9bb2a2f38d1d6f745
Jakub Ferjak <jakub.ferjak@gmail.com>
2020-10-26 13:02:15 +0100

    Add ObservableObject
    * ObservableObject implements the INotifyPropertyChanged interface to make updating properties in ViewModels easier

---
+++ calendar/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace calendar
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(sender, e);
+        }
+    }
+}
