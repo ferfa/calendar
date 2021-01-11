7c3e5f6af1fb351b165512d16c999901edbf4de3
Jakub Ferjak <jakub.ferjak@gmail.com>
2021-01-11 23:11:03 +0100

    Add RepeatingTaskModel class

---
+++ calendar/Models/RepeatingTaskModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calendar.Models
+{
+    public class RepeatingTaskModel : ObservableObject
+    {
+        private DateTime _startDate;
+        private DateTime _endDate;
+
+        public RepeatingTaskModel(DateTime startDate, DateTime endDate)
+        {
+            _startDate = startDate;
+            _endDate = endDate;
+        }
+    }
+}
